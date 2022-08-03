9d6502793821daf5ac35f0ce9f7edb9362164bad
shiraShr <shirasharabani@gmail.com>
2022-08-03 14:00:32 +0300

    ooooooof

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get, Post } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User, userSchema } from "src/schemas/user.schema";
-import { UserService } from "./user.service";
+//import { UserService } from "./user.service";
 
 @Controller('user')
 export class UserController{
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+    //constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
     
 
     @Post()
@@ -14,20 +14,20 @@ export class UserController{
         userSchema.add(newUser);
         return {newUser};
     }
-    @Get()
-    getAllUsers(){  
-        const results = findAllUsers();
-        return results;
-    };
+    // @Get()
+    // getAllUsers(){  
+    //     const results = findAllUsers();
+    //     return results;
+    // };
     
 
-    @Get()
-    getUser(id:string){
-        userSchema.get(id);
-        //  users.forEach(element => {       
-        //  });
+    // @Get()
+    // getUser(id:string){
+    //     userSchema.get(id);
+    //     //  users.forEach(element => {       
+    //     //  });
       
-      }
+    //   }
 }
 
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -18,22 +18,22 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/user.schema';
-import { User, UserDocument } from './schemas/user.schema';
-import { CreateUserDto } from './dto/create-user.dto';
+//import { User, UserDocument } from './schemas/user.schema';
+//import { CreateUserDto } from './dto/create-user.dto';
 
-@Injectable()
-export class CatsService {
-  constructor(@InjectModel(User.name) private catModel: Model<UserDocument>) {}
+// @Injectable()
+// export class CatsService {
+//   //constructor(@InjectModel(User.name) private catModel: Model<UserDocument>) {}
 
-  async create(createCatDto: CreateUserDto): Promise<User> {
-    const createdCat = new this.catModel(createCatDto);
-    return createdCat.save();
-  }
+//   async create(createCatDto: CreateUserDto): Promise<User> {
+//     const createdCat = new this.catModel(createCatDto);
+//     return createdCat.save();
+//   }
 
-  async findAll(): Promise<User[]> {
-    return this.catModel.find().exec();
-  }
-}
+//   async findAll(): Promise<User[]> {
+//     return this.catModel.find().exec();
+//   }
+// }
 
 
 
