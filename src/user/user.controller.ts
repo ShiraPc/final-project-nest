59ea34ad545593647e2902e1cbba9f9ded8635c4
yehuditCohen020302 <yehudit8824@gmail.com>
2022-08-09 13:43:30 +0300

    add func update and findOne

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { User } from './user.model';
 import { UserService } from "./user.service";
 
@@ -15,6 +15,16 @@ export class UserController {
     @Get()
     getAllUsers() {
         return this.userService.findAll();
+    };
+
+    @Get('/:id')
+    getOneUsers(@Param('id') id) {
+        return this.userService.findOne(id);
+    };
+    
+    @Put('/:id')
+    updateUser(@Body() updateUser:User, @Param('id') id:number) {
+        return this.userService.update(updateUser , id);
     };
 
 }
--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,21 +1,29 @@
-import { IsEmail, IsEmpty, IsNotEmpty, Length } from "class-validator";
+import { IsEmail, IsEmpty, IsNotEmpty, IsPhoneNumber, Length } from "class-validator";
 import * as mongoose from "mongoose";
 
 export const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    uid: { type: String, required: true },
+    role: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phone: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true },
 });
 
 export class User {
-    id: string;
+    uid: string;
+    role: string;
     @IsNotEmpty() 
     @Length(2) 
-    name: string;
+    firstName: string;
+    @IsNotEmpty() 
+    @Length(2) 
+    lastName: string;
+    // @IsPhoneNumber()
+    phone: string;  
     @IsEmail()
     @IsNotEmpty()  
     email: string;
-    @IsNotEmpty()  
-    @Length(4)
-    password: string;
+   
 }
+    
