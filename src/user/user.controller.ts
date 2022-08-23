5e7a7f320f56d0e1848bb3c8f615838a4779798b
shiraShr <shirasharabani@gmail.com>
2022-08-23 16:14:29 +0300

    lol

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,41 +1,21 @@
-
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { User } from "./user.model";
-// import { User, userSchema } from "src/schemas/user.schema";
-// //import { UserService } from "./user.service";
-
-// import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-// import { User } from './user.model';
+import { User } from './user.model';
 import { UserService } from "./user.service";
 
-
 @Controller('user')
-export class UserController{
- 
-    //constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
-    
+export class UserController {
+
+    constructor(private userService: UserService) {
+    }
 
     @Post()
     createUser(@Body() newUser: User) {
         this.userService.create(newUser);
     }
-    // @Get()
-    // getAllUsers(){  
-    //     const results = findAllUsers();
-    //     return results;
-    // };
-    
-
-
-    // @Get()
-    // getUser(id:string){
-    //     userSchema.get(id);
-    //     //  users.forEach(element => {       
-    //     //  });
-      
-    //   }
+    @Get()
+    getAllUsers() {
+        return this.userService.findAll();
+    };
 
     @Get('/:id')
     getOneUsers(@Param('id') id:string) {
@@ -52,7 +32,13 @@ export class UserController{
         return this.userService.delete(id);
     }
 
-
 }
+
+
+
+
+
+
+
 
 
