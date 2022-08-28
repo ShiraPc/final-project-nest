87ca1c53c785411b42d0e8c4aea20a0206067f6e
yehuditCohen020302 <yehudit8824@gmail.com>
2022-08-29 00:13:21 +0300

    create location module...

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './db/database.module';
 import { UserModule } from './user/user.module';
-import { SystemController } from './system/system.controller';
-import { SystemService } from './system/system.service';
 import { SystemModule } from './system/system.module';
+import { LocationModule } from './location/location.module';
 
 
 
 @Module({
 
-  imports: [AuthModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/finalProjectDB'),UserModule, SystemModule],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/finalProjectDB'),UserModule, SystemModule, LocationModule],
   controllers: [],
   providers: [],
 
